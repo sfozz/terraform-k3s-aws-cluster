8c85dffe80b31e9cc3e6162255b62cb0b48f31c1
Taylor Price <tayworm@gmail.com>
2021-10-25 16:38:19 -0700

    bump default version of rancher to understand bootstrapPassword

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "rancher_password" {
 
 variable "rancher_version" {
   type        = string
-  default     = "2.6.1"
+  default     = "2.6.2"
   description = "Version of Rancher to install"
 }
 
